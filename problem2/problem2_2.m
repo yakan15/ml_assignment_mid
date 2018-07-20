d43fe79a24b7fa529652992741a5d1122e2a0ec5
Miyazawa Hajime <h.m_92e3i1@hotmail.com>
2018-07-20 14:15:12 +0900

    fix problem2_2

--- problem2/problem2_2.m
+++ problem2/problem2_2.m
@@ -4,7 +4,7 @@ function [weight,weight_acc] = problem2_2(mu,A,lambda)
 close all;
 cvx_begin;
 variables w1 w2;
-minimize(([w1;w2]-[1;2])'*[3 0.5;0.5 1]*([w1;w2]-[1;2])+lambda*(abs(w1)+abs(w2)));
+minimize(([w1;w2]-mu)'*A*([w1;w2]-mu)+lambda*(abs(w1)+abs(w2)));
 cvx_end;
 
 weight = problem2(mu,A,lambda,[w1;w2],0);
